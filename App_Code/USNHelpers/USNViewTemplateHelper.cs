23fb67974823f0b9c84e272fe782cda6970e9f90
kyle-grogan <39771800+kyle-grogan@users.noreply.github.com>
2018-05-30 15:18:56 -0300

    Update USNViewTemplateHelper.cs

--- App_Code/USNHelpers/USNViewTemplateHelper.cs
+++ App_Code/USNHelpers/USNViewTemplateHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Umbraco.Core.Models;
 using USNStarterKit.USNEntities;
 using USNStarterKit.USNBackgroundOptions.Models;
@@ -15,47 +15,63 @@ namespace USNStarterKit.USNHelpers
             {
                 switch (backgroundColor)
                 {
-                    case "8bc435":
+                    //if background c1, c1 background + c2 text
+                    //redorange - black
+                    case "ec6425":
                         settings.BackGroundStyle = "c1-bg";
-                        settings.HeadingStyle = "c5-text";
-                        settings.TextStyle = "c5-text";
+                        settings.HeadingStyle = "c2-text";
+                        settings.TextStyle = "c2-text";
                         break;
-                    case "000000":
+                    //if background c2, c2 background + c1 text
+                    //black - redorange
+                    case "101212":
                         settings.BackGroundStyle = "c2-bg";
-                        settings.HeadingStyle = "c5-text";
-                        settings.TextStyle = "c5-text";
+                        settings.HeadingStyle = "c1-text";
+                        settings.TextStyle = "c1-text";
                         break;
-                    case "2c2d2f":
+                    //if background c3, c3 background + c4 text
+                    //blue - gray
+                    case "46bea8":
                         settings.BackGroundStyle = "c3-bg";
-                        settings.HeadingStyle = "c5-text";
-                        settings.TextStyle = "c5-text";
+                        settings.HeadingStyle = "c4-text";
+                        settings.TextStyle = "c4-text";
                         break;
-                    case "f4f4f4":
+                    //if background c4, c4 background + c3 text
+                    //gray - blue
+                    case "e3ded4":
                         settings.BackGroundStyle = "c4-bg";
                         settings.HeadingStyle = "c3-text";
                         settings.TextStyle = String.Empty;
                         break;
-                    case "ffffff":
+                    //if background c5, c5 background + c6 text
+                    //dark gray - gray
+                    case "3a342e":
                         settings.BackGroundStyle = "c5-bg";
-                        settings.HeadingStyle = "c3-text";
+                        settings.HeadingStyle = "c6-text";
                         settings.TextStyle = String.Empty;
                         break;
-                    case "e1e2e5":
+                    //if background c6, c6 background + c5 text
+                    //gray - dark gray
+                    case "6f6862":
                         settings.BackGroundStyle = "c6-bg";
-                        settings.HeadingStyle = "c3-text";
+                        settings.HeadingStyle = "c5-text";
                         settings.TextStyle = String.Empty;
                         break;
+                    //if no background selected, use primary colors
+                    //if no background, c1 background + c2 text
                     default:
-                        settings.BackGroundStyle = "c5-bg";
-                        settings.HeadingStyle = "c3-text";
+                        settings.BackGroundStyle = "c1-bg";
+                        settings.HeadingStyle = "c2-text";
                         settings.TextStyle = String.Empty;
                         break;
                 }
             }
+            //if string is null or empty, use default
+            //c1 background + c2 text
             else
             {
-                settings.BackGroundStyle = "c5-bg";
-                settings.HeadingStyle = "c3-text";
+                settings.BackGroundStyle = "c1-bg";
+                settings.HeadingStyle = "c2-text";
                 settings.TextStyle = String.Empty;
             }
 
@@ -63,32 +79,48 @@ namespace USNStarterKit.USNHelpers
             {
                 switch (buttonColour)
                 {
-                    case "8bc435":
-                        settings.ButtonStyle = "c1-bg c5-text";
+                    //if button c1, c1 button + c2 text
+                    //button redorange - black
+                    case "ec6425":
+                        settings.ButtonStyle = "c1-bg c2-text";
                         break;
-                    case "000000":
-                        settings.ButtonStyle = "c2-bg c5-text";
+                    //if button c2, c2 button + c1 text
+                    //button black - redorange
+                    case "101212":
+                        settings.ButtonStyle = "c2-bg c1-text";
                         break;
-                    case "2c2d2f":
-                        settings.ButtonStyle = "c3-bg c5-text";
+                    //if button c3, c3 button + c4 text
+                    //button blue - gray
+                    case "46bea8":
+                        settings.ButtonStyle = "c3-bg c4-text";
                         break;
-                    case "f4f4f4":
-                        settings.ButtonStyle = "c4-bg c1-text";
+                    //if button c4, c4 button + c3 text
+                    //button gray - blue
+                    case "e3ded4":
+                        settings.ButtonStyle = "c4-bg c3-text";
                         break;
-                    case "ffffff":
-                        settings.ButtonStyle = "c5-bg c1-text";
+                    //if button c5, c5 button + c6 text
+                    //button dark gray - gray
+                    case "3a342e":
+                        settings.ButtonStyle = "c5-bg c6-text";
                         break;
-                    case "e1e2e5":
-                        settings.ButtonStyle = "c6-bg c3-text";
+                    //if button c6, c6 button + c5 text
+                    //button gray - dark gray
+                    case "6f6862":
+                        settings.ButtonStyle = "c6-bg c5-text";
                         break;
+                    //if no button color selected, c2 button + c1 text
+                    //button orange - black
                     default:
-                        settings.ButtonStyle = "c1-bg c5-text";
+                        settings.ButtonStyle = "c2-bg c1-text";
                         break;
                 }
             }
+            //if button null or empty, default button
+            //button orange - black
             else
             {
-                settings.ButtonStyle = "c1-bg c5-text";
+                settings.ButtonStyle = "c2-bg c1-text";
             }
 
             return settings;
